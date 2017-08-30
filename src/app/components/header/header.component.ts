0f7bda791e8dad800581f705850dfae740bfa30d
Emmanuel Mancilla <al116584@edu.uaa.mx>
2017-08-30 18:31:34 -0500

    New fonts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { InfoService } from '../../services/info.service'
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
